a112a94c66e1ab67133c55f7c55b2789a07045fd
tahmidurrahmann <138325180+tahmidurrahmann@users.noreply.github.com>
2023-12-21 14:50:14 +0600

    added task post, delete, put and get method

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process?.env?.DB_USER}:${process?.env?.DB_PASS}@cluster0.glcj3l3.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -21,7 +21,40 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+    const taskCollection = client.db("jobTask").collection("tasks");
+
+    app.post("/createTask", async (req, res) => {
+      const taskData = req?.body;
+      const result = await taskCollection.insertOne(taskData);
+      res.send(result);
+    })
+
+    app.get("/previousTask", async (req, res) => {
+      const result = await taskCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.delete("/taskDelete/:id", async (req, res) => {
+      const id = req?.params?.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.put("/updateTask/:id", async (req, res) => {
+      const updatedData = req?.body;
+      const id = req?.params?.id;
+      const filter = {_id : new ObjectId(id)};
+      const options = { upsert: true };
+      const updateDoc = {
+        $set : {
+          ...updatedData
+        }
+      }
+      const result = await taskCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
@@ -31,9 +64,9 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("Job Task Server is getting me a Job")
+  res.send("Job Task Server is getting me a Job")
 })
 
 app.listen(port, () => {
-    console.log(`Job Task Server is getting me a job on port ${port}`);
+  console.log(`Job Task Server is getting me a job on port ${port}`);
 })
